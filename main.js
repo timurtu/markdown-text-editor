8b15d0a8561afb5eebc684b950f1e35c83f7fd1f
Timur Uzel <timurt.uzel@gmail.com>
2016-02-14 23:54:46 -0500

    Better file reading

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const fs = require('fs');
 let mainWindow = null;
+const ipcMain = require('electron').ipcMain;
+const autoSavePath = './res/autosave.md';
+
+ipcMain.on('asynchronous-message', function(event, message) {
+  console.log(message); // prints "ping"
+  fs.readFile(autoSavePath, function(err, data) {
+    if (err) throw err;
+    event.sender.send('asynchronous-reply', data);
+  });
+});
+
+function readFile(file) {
+  fs.readFile(file, function(err, data) {
+    if (err) throw err;
+    return data;
+  });
+}
 
 function writeFile(file, data) {
   fs.writeFile(file, data, (fileError) => {
--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
-var editor;
-var remote = require('remote');
+'use strict';
+
+var editor = document.getElementById('markdown-text');
+const remote = require('remote');
 // const remote = require('electron').remote;
 const BrowserWindow = remote.BrowserWindow;
-var fs = require('fs');
+const fs = require('fs');
+const ipcRenderer = require('electron').ipcRenderer;
 
-window.onbeforeunload = function() {
-  return "Are you sure?";
-}
-
-function writeFile(file, data) {
-  fs.writeFile(file, data, fileError => {
-    if (fileError) throw fileError;
-    console.log(`Markdown was written to ${file}!`);
-  });
-}
-
-function readFile(file) {
-  editor = document.getElementById('markdown-text')
-  fs.readFile(file, function(err, data) {
-    if (err) throw err;
-    editor.innerHTML = data;
-  });
-}
-
-readFile('./res/autosave.md');
-
-// app.on('close', function() {
-//   editor = document.getElementById('markdown-text');
-//   writeFile('./res/autosave.md', editor.innerHTML);
-// });
-
-
-
-// writes to the autosave which loads automatically
-// writeFile('./res/autosave.md', 'this will be markdown\n');
+ipcRenderer.on('asynchronous-reply', function(event, reply) {
+  editor.innerHTML = reply;
+});
+ipcRenderer.send('asynchronous-message', 'ping');
