f2c4e97d76625393e3379a2127e4bb46cf77a3b1
Timur Uzel <timurt.uzel@gmail.com>
2016-02-15 17:13:10 -0500

    why was npm a dependency...

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var editor = document.getElementById('markdown-text');
-const remote = require('remote');
+const remote = require('electron').remote;
 // const remote = require('electron').remote;
 const BrowserWindow = remote.BrowserWindow;
 const fs = require('fs');
 const ipcRenderer = require('electron').ipcRenderer;
 
-ipcRenderer.on('asynchronous-reply', function(event, reply) {
+ipcRenderer.on('asynchronous-reply', (event, reply) => {
   editor.innerHTML = reply;
 });
 ipcRenderer.send('asynchronous-message', 'ping');
