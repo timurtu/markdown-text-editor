1257ff4be52427dea9ba11eac6276338ae8253ce
Timur Uzel <timurt.uzel@gmail.com>
2016-02-15 22:14:04 -0500

    failing at making a bold button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 'use strict';
 
-var editor = document.getElementById('markdown-text');
+const editor = document.getElementById('markdown-text');
+const makeBold = document.getElementById('make-bold');
 const remote = require('electron').remote;
 const ipcRenderer = require('electron').ipcRenderer;
 
-ipcRenderer.on('asynchronous-reply', function(event, reply) {
+ipcRenderer.on('asynchronous-reply', function (event, reply) {
   editor.textContent = reply;
 });
 ipcRenderer.send('asynchronous-message', 'ping');
+
+function getSelectedText() {
+  const length = editor.selectionEnd - editor.selectionStart;
+  if (length > 0) {
+    return editor.textContent.substring(editor.selectionStart, editor.selectionEnd);
+  }
+}
+
+makeBold.onclick = function () {
+
+};
