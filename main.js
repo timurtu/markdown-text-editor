c27e1b6b75c303b4628686faef32d341487fe4c9
Timur Uzel <Timur Uzel>
2016-02-29 01:27:19 -0500

    not sure what this bug is on windows, cannot read property on of undefined

--- main.js
+++ main.js
@@ -2,26 +2,26 @@
 
 const electron = require('electron');
 const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = require('electron').BrowserWindow;
 const fs = require('fs');
 let mainWindow = null;
 const ipcMain = require('electron').ipcMain;
 const autoSavePath = './res/autosave.md';
 
-ipcMain.on('asynchronous-message', function(event, message) {
-  console.log(message);
-  fs.readFile(autoSavePath, function(err, data) {
-    if (err) throw err;
-    event.sender.send('asynchronous-reply', data);
-  });
-});
+//ipcMain.on('asynchronous-message', function(event, message) {
+//  console.log(message);
+//  fs.readFile(autoSavePath, function(err, data) {
+//    if (err) throw err;
+//    event.sender.send('asynchronous-reply', data);
+//  });
+//});
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 600,
     height: 600
   });
-  mainWindow.loadURL('file://' + __dirname + '/src/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.webContents.openDevTools();
   mainWindow.on('closed', () => {
     mainWindow = null;
--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ const makeBold = document.getElementById('make-bold');
 const remote = require('electron').remote;
 const ipcRenderer = require('electron').ipcRenderer;
 
-ipcRenderer.on('asynchronous-reply', function (event, reply) {
-  editor.textContent = reply;
-});
-ipcRenderer.send('asynchronous-message', 'ping');
+//ipcRenderer.on('asynchronous-reply', function (event, reply) {
+//  editor.textContent = reply;
+//});
+//ipcRenderer.send('asynchronous-message', 'ping');
 
-function getSelectedText() {
-  const length = editor.selectionEnd - editor.selectionStart;
-  if (length > 0) {
-    return editor.textContent.substring(editor.selectionStart, editor.selectionEnd);
-  }
-}
-
-makeBold.onclick = function () {
-
-};
+//function getSelectedText() {
+//  const length = editor.selectionEnd - editor.selectionStart;
+//  if (length > 0) {
+//    return editor.textContent.substring(editor.selectionStart, editor.selectionEnd);
+//  }
+//}
+//
+//makeBold.onclick = function () {
+//
+//};
