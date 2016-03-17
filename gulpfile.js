5a7b01f897751b296bacaa5729eabc99378948d1
Timur Uzel <Timur Uzel>
2016-03-16 22:42:00 -0400

    only compile source files if they changed

--- gulpfile.js
+++ gulpfile.js
@@ -8,24 +8,43 @@ var sass = require('gulp-sass');
 
 gulp.task('default', ['main', 'js', 'sass']);
 
+/**
+ * Transpile the main javascript file separately because
+ * it is required in the root of an electron app.
+ * */
 gulp.task('main', () => {
+
+  const DEST = './';
+
   return gulp.src('src/main.js')
+    .pipe(changed(DEST))
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest(DEST));
+
 });
 
 gulp.task('js', () => {
+
+  const DEST = './dist/js';
+
   return gulp.src('src/js/**/*.js')
+    .pipe(changed(DEST))
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest(DEST));
+
 });
 
 gulp.task('sass', () => {
+
+  const DEST = './dist/css';
+
   return gulp.src('src/sass/**/*.scss')
+    .pipe(changed(DEST))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest(DEST));
+
 });
