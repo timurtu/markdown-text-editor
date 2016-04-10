e3f8f1d7a0453223f025eb92484cb6d43177a756
Timur Uzel <timurt.uzel@gmail.com>
2016-04-09 20:01:24 -0400

    rendering html in a dirty way

--- src/js/content.js
+++ src/js/content.js
@@ -1,22 +1,8 @@
-'use strict';
-
 const editor = document.getElementById('markdown-text');
-const makeBold = document.getElementById('make-bold');
-const remote = require('electron').remote;
-const ipcRenderer = require('electron').ipcRenderer;
+const fs = require('fs');
+const paths = {
+    autosaveMD: './res/autosave.md',
+    autosaveHTML: './res/autosave.html'
+};
 
-//ipcRenderer.on('asynchronous-reply', function (event, reply) {
-//  editor.textContent = reply;
-//});
-//ipcRenderer.send('asynchronous-message', 'ping');
-
-//function getSelectedText() {
-//  const length = editor.selectionEnd - editor.selectionStart;
-//  if (length > 0) {
-//    return editor.textContent.substring(editor.selectionStart, editor.selectionEnd);
-//  }
-//}
-//
-//makeBold.onclick = function () {
-//
-//};
+editor.innerHTML = fs.readFileSync(paths.autosaveHTML);
